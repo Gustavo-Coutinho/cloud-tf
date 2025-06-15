ea6129a6a8a2ecbc0c9e73f01e55c9487ff18d1b
Gustavo Coutinho <gupoco@outlook.com>
2025-06-15 01:31:02 -0300

    feat: alterar para CIDR dinâmico

--- aws-eks/vpc.tf
+++ aws-eks/vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  # cidr_block           = "10.0.0.0/16"
+  cidr_block           = cidrsubnet("10.0.0.0/16", 8, 1)
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
