239357ac1513cd59d286b72d7f6040bd06118282
Gustavo Coutinho <gupoco@outlook.com>
2025-06-15 16:56:25 -0300

    fix: ajustar destroy para o s3

--- aws-prevent-destroy/s3-terraform-state.tf
+++ aws-prevent-destroy/s3-terraform-state.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "ctf_terraform_state_gupoco" {
-  bucket = "ctf-terraform-state-gupoco"
-  region = "us-east-1"
+  bucket        = "ctf-terraform-state-gupoco"
+  region        = "us-east-1"
+  force_destroy = true
   tags = {
     Name = "ctf-terraform-state-gupoco"
   }
