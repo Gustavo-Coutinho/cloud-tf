5a283e9d68c7cea201e6b73d59bcc1d375311be7
Gustavo Coutinho <gupoco@outlook.com>
2025-06-15 18:18:21 -0300

    feat: obter CIDR a partir de variável definida no terraform.tfvars

--- aws-eks/vpc.tf
+++ aws-eks/vpc.tf
@@ -1,6 +1,5 @@
-resource "aws_vpc" "main" {
-  # cidr_block           = "10.0.0.0/16"
-  cidr_block           = cidrsubnet("10.0.0.0/16", 8, 1)
+resource "aws_vpc" "ctf_eks_vpc" {
+  cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
