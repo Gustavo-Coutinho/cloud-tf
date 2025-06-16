c350aaef7a88f4c7d94f3d8b0ec2f15eac6ae0d5
Gustavo Coutinho <gupoco@outlook.com>
2025-06-15 21:30:56 -0300

    feat: add route table privada para uso do NAT Gateway

---
+++ aws-eks/route_table_private.tf
@@ -0,0 +1,29 @@
+# para uso do NAT Gateway
+resource "aws_route_table" "ctf_eks_route_table_private-1a" {
+  vpc_id = aws_vpc.ctf_eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ctf_eks_ngw_1a.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-rtb-private-1a"
+  })
+}
+resource "aws_route_table" "ctf_eks_route_table_private-1b" {
+  vpc_id = aws_vpc.ctf_eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ctf_eks_ngw_1b.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-rtb-private-1b"
+  })
+}
