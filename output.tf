132f2849e3195c84a56e91f67b2d84ca9eeda862
Gustavo Coutinho <gupoco@outlook.com>
2025-06-16 23:54:45 -0300

    feat: output para debug

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "saida_ctf_eks_subnet_public_1a" {
+  value = module.ctf_eks_network.subnet_pub_1a
+}
+output "saida_ctf_eks_subnet_public_1b" {
+  value = module.ctf_eks_network.subnet_pub_1b
+}
+output "saida_ctf_eks_subnet_private_1a" {
+  value = module.ctf_eks_network.subnet_priv_1a
+}
+output "saida_ctf_eks_subnet_private_1b" {
+  value = module.ctf_eks_network.subnet_priv_1b
+}
