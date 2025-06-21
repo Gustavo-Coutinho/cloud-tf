47f1c64a184b0214638932e310b83328a7e17cd6
Gustavo Coutinho <gupoco@outlook.com>
2025-06-21 00:00:44 -0300

    feat: adicionar OIDC provider ao cluster EKS

---
+++ modules/cluster/oidc.tf
@@ -0,0 +1,11 @@
+data "tls_certificate" "ctf_eks_oidc_tls_certificate" {
+  url = aws_eks_cluster.ctf_eks_cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "ctf_eks_oidc" {
+  client_id_list = [
+    "sts.amazon.com"
+  ]
+  thumbprint_list = data.tls_certificate.ctf_eks_oidc_tls_certificate.certificates[*].sha1_fingerprint
+  url             = data.tls_certificate.ctf_eks_oidc_tls_certificate.url
+}
