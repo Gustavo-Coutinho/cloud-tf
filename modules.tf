a11cc7c149f508559dbc77675075740f7f4ea335
Gustavo Coutinho <gupoco@outlook.com>
2025-06-21 00:08:15 -0300

    feat: adicionar EKS para provisionamento de cluster Kubernetes

--- modules.tf
+++ modules.tf
@@ -5,6 +5,14 @@ module "ctf_eks_network" {
   tags         = local.tags
 }
 
+module "ctf_eks_cluster" {
+  source           = "./modules/cluster"
+  project_name     = var.project_name
+  tags             = local.tags
+  public_subnet_1a = module.ctf_eks_network.subnet_pub_1a
+  public_subnet_1b = module.ctf_eks_network.subnet_pub_1b
+}
+
 # module "tfstate-aws-s3-setup" {
 #   source = "./modules/utils/tfstate-aws-s3-setup"
 # }
