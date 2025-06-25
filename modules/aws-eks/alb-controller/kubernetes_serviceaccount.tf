57c8fe791868462a00d3cabb3bf860bb40ec8fa1
Gustavo Coutinho <gupoco@outlook.com>
2025-06-25 19:06:28 -0300

    feat: adicionar service account para o ALB Controller

---
+++ modules/aws-eks/alb-controller/kubernetes_serviceaccount.tf
@@ -0,0 +1,9 @@
+resource "kubernetes_service_account" "ctf_eks_kubernetes_controller_service_account" {
+  metadata {
+    name = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.ctf_eks_iam_alb_controller_role.arn
+    }
+  }
+}
