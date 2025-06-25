085e06114774f920a1d5b5d7ea49297c00c89ce4
Gustavo Coutinho <gupoco@outlook.com>
2025-06-25 19:16:24 -0300

    feat: adicionar ALB Controller via Helm

---
+++ modules/aws-eks/alb-controller/helm_alb_controller.tf
@@ -0,0 +1,25 @@
+# helm repo add eks-charts https://aws.github.io/eks-charts
+# helm repo update
+# helm search repo aws-load-balancer-controller
+resource "helm_release" "ctf_eks_helm_alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version = "1.13.3"
+  namespace = "kube-system"
+
+  set = [
+    {
+      name  = "clusterName"
+      value = var.cluster_name
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    }
+  ]
+}
