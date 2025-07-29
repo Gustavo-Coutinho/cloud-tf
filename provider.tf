37a3f6378ce51a88b2a9546917b97157b6c6ae30
Gustavo Coutinho <gupoco@outlook.com>
2025-07-28 22:53:49 -0300

    minor: especificar versão do provider helm

--- provider.tf
+++ provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.0.0-beta3"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.2"
+    }
 
   }
   backend "s3" {
