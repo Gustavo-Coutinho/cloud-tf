c305a60789b03dcd5504bccedd118ca2d8e25fe3
Gustavo Coutinho <gupoco@outlook.com>
2025-08-15 22:12:16 -0300

    chore: ajuste de terraform fmt

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "certificate_authority" {
-  value = module.ctf_eks_cluster.saida_certificate_authority
+  value       = module.ctf_eks_cluster.saida_certificate_authority
   description = "The certificate authority data for the EKS cluster."
 }
 
 output "endpoint" {
-  value = module.ctf_eks_cluster.saida_endpoint
+  value       = module.ctf_eks_cluster.saida_endpoint
   description = "The endpoint for the EKS cluster."
 }
